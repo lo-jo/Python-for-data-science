86702fa99daf245336d40b48ac9858cf4d079ecc
lorraine <lorraine.jospin@gmail.com>
2024-05-10 14:28:19 +0200

    [NEW] EX04 is done

---
+++ PY00/ex04/whatis.py
@@ -0,0 +1,36 @@
+# DESCRIPTION:
+# The Even and the Odd
+
+# Create a script that takes a number as argument, checks whether it is odd or even,
+# and prints the result.
+# If more than one argument is provided or if the argument is not an integer, print an
+# AssertionError.
+
+import sys
+print(sys.argv[0], len(sys.argv))
+
+
+
+try:
+    if len(sys.argv) != 2:
+        raise AssertionError("AssertionError: more than one argument is provided")
+    if not sys.argv[1].isdigit():
+        raise AssertionError("AssertionError: argument is not an integer")
+except AssertionError as e:
+    print(e)
+    exit()
+
+if (len(sys.argv) == 2):
+    arg = int(sys.argv[1])
+    # CHECK IF IT IS AN INT?????
+    if arg%2 == 0:
+        print("I am even")
+    else:
+        print("I am odd")
+
+# assert len(sys.argv) == 2, "more than one argument is provided"
+
+
+# if sys.argv.size()
+# for arg in sys.argv:
+#     print(arg)
