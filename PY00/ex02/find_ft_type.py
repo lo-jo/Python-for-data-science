4abee8142833a1e57f4bc49b216ce83ffef02ba1
lorraine <lorraine.jospin@gmail.com>
2024-05-10 13:59:10 +0200

    [EDIT] forgot formatting

--- PY00/ex02/find_ft_type.py
+++ PY00/ex02/find_ft_type.py
@@ -1,17 +1,18 @@
 # DESCRIPTION :
+# First function python
 # Write a function that prints the object types and returns 42.
 
 def all_thing_is_obj(object: any) -> int:
     if isinstance(object, str):
-        print(object, "is in the kitchen : <class 'str'>")
+        print(object, "is in the kitchen :", type(object))
     elif isinstance(object, list):
-        print("List : <class 'list'>")
+        print("List :", type(object))
     elif isinstance(object, tuple):
-        print("Tuple : <class 'tuple'>")
+        print("Tuple :", type(object))
     elif isinstance(object, set):
-        print("That is a set")
+        print("Set :", type(object))
     elif isinstance(object, dict):
-        print("That is a dict")
+        print("Dict :", type(object))
     else:
         print("Type not found")
     return 42
