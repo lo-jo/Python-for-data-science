e504def57932eb32db2d42fdbb49caed2ddb7819
lorraine <lorraine.jospin@gmail.com>
2024-05-13 14:23:13 +0200

    [EDIT] forgot scientific notation

--- PY00/ex01/format_ft_time.py
+++ PY00/ex01/format_ft_time.py
@@ -9,7 +9,7 @@
 import datetime
 
 now = datetime.datetime.now()
-print(now)
+# print(now)
 
 # Epoch time, also known as Unix time or POSIX time, 
 # is the amount of time in seconds that has elapsed since January 1st, 1970 (00:00:00 UTC).
@@ -18,7 +18,8 @@ timestamp = now.timestamp()
 # Formatted string literals (f-strings)
 # Format specifier :,4f to format float types with 4 decimals + add commas to separate thousands
 # see format() for flags
-print("Seconds since January 1, 1970:", f"{timestamp:,.4f}")
+print("Seconds since January 1, 1970:", f"{timestamp:,.4f} or {timestamp:.2e} in scientific notation")
+
 
 
 # see python strftime() for flags
